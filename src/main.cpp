9159d9bef903d9357b84c6a53ea19f48dd740a39
Tomasz Pawłowski <t.pawlowski03@gmail.com>
2024-03-19 22:47:59 +0100

    Extend code to four parallel tiles

--- src/main.cpp
+++ src/main.cpp
@@ -1,54 +1,80 @@
 #include <Arduino.h>
 #include <Servo.h>
 
-Servo myservo;  // create servo object to control a servo
+#define DEBUG 1
 
-// int potpin = 0;  // analog pin used to connect the potentiometer
-// int val;    // variable to read the value from the analog pin
+// Defining the count of sensors and motor pairs and their corresponding pins 
+// (depends on the layout)
+constexpr size_t n = 4; // count of sensor + servo pairs
+constexpr uint16_t servo_pin[n] = {11, 10, 6, 3}; // servo motor pins (must be PWM~)
+constexpr uint16_t photoresistor_pin[n] = {A0,A1,A2,A3}; // photoresitor pins (must be analog)
 
-// void setup() {
-//   // myservo.attach(9);  // attaches the servo on pin 9 to the servo object
+// Program specific variables and objects
+Servo servos[n];
+uint64_t last_lit[n] = {0,0,0,0};
+int last_servo_degrees[n] = {0,0,0,0};
+uint16_t temp_time = 0;
+uint16_t photoresistor_value = 0;
 
-//   pinMode(A0,INPUT);
-// }
-
-// void loop() {
-//   Serial.println(analogRead(A0));
-//   delay(100);
-//   // val = analogRead(potpin);            // reads the value of the potentiometer (value between 0 and 1023)
-//   // val = map(val, 0, 1023, 0, 180);     // scale it to use it with the servo (value between 0 and 180)
-//   // myservo.write(val);                  // sets the servo position according to the scaled value
-//   // delay(15);                           // waits for the servo to get there
-
-//   // myservo.write(0);
-//   // delay(100);
-//   // myservo.write(45);
-//   // delay(100);
-
-// }
+// Change parameters depending on empirical measurements
+constexpr uint16_t thresholds[n] = {30,30,30,30};
+constexpr uint64_t min_delay_ms = 100;
+constexpr int servo_pressed_degrees = 45;
+constexpr int servo_released_degrees = 0;
 
 
-int sensorPin = A0;   // select the input pin for the potentiometer
-int ledPin = 13;      // select the pin for the LED
-int sensorValue = 0;  // variable to store the value coming from the sensor
-
-void setup() {
+void setup()
+{
+  #ifdef DEBUG
+  // Opening serial port for calibration and debugging
   Serial.begin(9600);
-  myservo.attach(9);
-  // declare the ledPin as an OUTPUT:
-  pinMode(ledPin, OUTPUT);
+  #endif
+  
+  //Assingning Analog pins to read data from photoresistor
+  pinMode(photoresistor_pin[0], INPUT);
+  pinMode(photoresistor_pin[1], INPUT);
+  pinMode(photoresistor_pin[2], INPUT);
+  pinMode(photoresistor_pin[3], INPUT);
+  
+  // Attaching servo pins to allow for control
+  servos[0].attach(servo_pin[0]);
+  servos[1].attach(servo_pin[1]);
+  servos[2].attach(servo_pin[2]);
+  servos[3].attach(servo_pin[3]);
 }
 
 void loop() {
-  // read the value from the sensor:
-  sensorValue = analogRead(sensorPin);
-  if (sensorValue < 30) {
-    digitalWrite(ledPin, HIGH);
-    Serial.println(sensorValue);
-    myservo.write(0);
-  } else {
-    digitalWrite(ledPin, LOW);
-    myservo.write(45);
+  // For each pair `i` we check sequentially that ...
+  for (size_t i=0; i<n; i++){
+
+    // if the time since last value read for that specific photoresistor `i` was read
+    // is less than min_delay_ms
+    if ( ((temp_time = millis()) - last_lit[i]) > min_delay_ms ) {
+      last_lit[i] = temp_time; // reset timer
+
+      // check if the photoresistor `i` value is less than the threshold parameter
+      // Note that the threshold value should be adjusted 
+      // In other words, this if clause detects black tiles
+      if ( (photoresistor_value = analogRead(photoresistor_pin[i])) < thresholds[i]) {
+            
+        // Print the value read by photoresistor to the serial monitor
+        #ifdef DEBUG
+        Serial.print(i);
+        Serial.print(" ");
+        Serial.print(photoresistor_value);
+        #endif
+
+        // Move the servomotor handle to press on the screen (45° default)
+        servos[i].write(servo_pressed_degrees);
+        last_servo_degrees[i] = servo_pressed_degrees;
+      
+      } else {
+        // Move back the servomotor handle to release the tap (0° default)
+        servos[i].write(servo_released_degrees);
+        last_servo_degrees[i] = servo_released_degrees;
+      }
+    }
+    // save last handle position until the next check
+    servos[i].write(last_servo_degrees[i]);
   }
-  delay(10);
 }

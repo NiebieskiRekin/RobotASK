2fd27a6789cc5fb3d95a48b03cb388c666665164
Tomasz Pawłowski <t.pawlowski03@gmail.com>
2024-04-28 16:16:17 +0200

    Poprawki kodu, zwiększenie otworów na komponenty na 1mm, nowe wydruki do termotransferu i nawiercania otworów, poukładanie nieaktualnych wydruków UV

--- src/coins.cpp
+++ src/coins.cpp
@@ -1,76 +1,76 @@
-#define DEBUG 1
+// #define DEBUG 1
 
-#include <Arduino.h>
+// #include <Arduino.h>
 
-// Defining the count of sensors and motor pairs and their corresponding pins
-// (depends on the layout)
-constexpr size_t n = 4;                                     // count of sensor + servo pairs
-constexpr uint16_t coin_pin[n] = {6, 9, 10, 11};           // servo motor pins (must be PWM~)
-constexpr uint16_t photoresistor_pin[n] = {A0, A1, A2, A3}; // photoresitor pins (must be analog)
+// // Defining the count of sensors and motor pairs and their corresponding pins
+// // (depends on the layout)
+// constexpr size_t n = 4;                                     // count of sensor + servo pairs
+// constexpr uint16_t coin_pin[n] = {6, 9, 10, 11};           // servo motor pins (must be PWM~)
+// constexpr uint16_t photoresistor_pin[n] = {A0, A1, A2, A3}; // photoresitor pins (must be analog)
 
-// Program specific variables and objects
-uint64_t last_lit[n] = {0, 0, 0, 0};
-int last_coin_voltage[n] = {0,0,0,0};
-uint16_t temp_time = 0;
-uint16_t photoresistor_value = 0;
+// // Program specific variables and objects
+// uint64_t last_lit[n] = {0, 0, 0, 0};
+// int last_coin_voltage[n] = {0,0,0,0};
+// uint16_t temp_time = 0;
+// uint16_t photoresistor_value = 0;
 
-// Change parameters depending on empirical measurements
-constexpr uint16_t thresholds[n] = {500, 500, 500, 500};
-constexpr uint64_t min_delay_ms = 100;
+// // Change parameters depending on empirical measurements
+// constexpr uint16_t thresholds[n] = {500, 500, 500, 500};
+// constexpr uint64_t min_delay_ms = 100;
 
-void setup()
-{
-#ifdef DEBUG
-  // Opening serial port for calibration and debugging
-  Serial.begin(9600);
-#endif
+// void setup()
+// {
+// #ifdef DEBUG
+//   // Opening serial port for calibration and debugging
+//   Serial.begin(9600);
+// #endif
 
-  for (int i=0; i<n; i++){
-    // Assingning Analog pins to read data from photoresistor
-    pinMode(photoresistor_pin[i], INPUT);
-    // Assinging pins to coins to allow for screen control
-   pinMode(coin_pin[i],OUTPUT);
-  }    
+//   for (int i=0; i<n; i++){
+//     // Assingning Analog pins to read data from photoresistor
+//     pinMode(photoresistor_pin[i], INPUT);
+//     // Assinging pins to coins to allow for screen control
+//    pinMode(coin_pin[i],OUTPUT);
+//   }
 
-}
+// }
 
-void loop()
-{
-  // For each pair `i` we check sequentially that ...
-  for (size_t i = 0; i < n; i++)
-  {
+// void loop()
+// {
+//   // For each pair `i` we check sequentially that ...
+//   for (size_t i = 0; i < n; i++)
+//   {
 
-    // if the time since last value read for that specific photoresistor `i` was read
-    // is less than min_delay_ms
-    if (((temp_time = millis()) - last_lit[i]) > min_delay_ms)
-    {
-      last_lit[i] = temp_time; // reset timer
+//     // if the time since last value read for that specific photoresistor `i` was read
+//     // is less than min_delay_ms
+//     if (((temp_time = millis()) - last_lit[i]) > min_delay_ms)
+//     {
+//       last_lit[i] = temp_time; // reset timer
 
-      // check if the photoresistor `i` value is less than the threshold parameter
-      // Note that the threshold value should be adjusted
-      // In other words, this if clause detects black tiles
-      if ((photoresistor_value = analogRead(photoresistor_pin[i])) < thresholds[i])
-      {
+//       // check if the photoresistor `i` value is less than the threshold parameter
+//       // Note that the threshold value should be adjusted
+//       // In other words, this if clause detects black tiles
+//       if ((photoresistor_value = analogRead(photoresistor_pin[i])) < thresholds[i])
+//       {
 
-// Print the value read by photoresistor to the serial monitor
-#ifdef DEBUG
-        Serial.print(i);
-        Serial.print(" ");
-        Serial.print(photoresistor_value);
-#endif
+// // Print the value read by photoresistor to the serial monitor
+// #ifdef DEBUG
+//         Serial.print(i);
+//         Serial.print(" ");
+//         Serial.print(photoresistor_value);
+// #endif
 
-        // Apply voltage to a coin
-        digitalWrite(coin_pin[i],HIGH);
-        last_coin_voltage[i] = HIGH;
-      }
-      else
-      {
-        // Drop the voltage to release the tap
-        digitalWrite(coin_pin[i],LOW);
-        last_coin_voltage[i] = LOW;
-      }
-    }
-    // save last position until the next check
-    digitalWrite(coin_pin[i],last_coin_voltage[i]);
-  }
-}
+//         // Apply voltage to a coin
+//         digitalWrite(coin_pin[i],HIGH);
+//         last_coin_voltage[i] = HIGH;
+//       }
+//       else
+//       {
+//         // Drop the voltage to release the tap
+//         digitalWrite(coin_pin[i],LOW);
+//         last_coin_voltage[i] = LOW;
+//       }
+//     }
+//     // save last position until the next check
+//     digitalWrite(coin_pin[i],last_coin_voltage[i]);
+//   }
+// }
--- src/main.cpp
+++ src/main.cpp
@@ -4,23 +4,35 @@
 #include <Servo.h>
 
 // Defining the count of sensors and motor pairs and their corresponding pins
-// (depends on the layout)
+// (Change only if the layout of the board has changed)
 constexpr size_t n = 4;                                     // count of sensor + servo pairs
 constexpr uint16_t servo_pin[n] = {6, 9, 10, 11};           // servo motor pins (must be PWM~)
 constexpr uint16_t photoresistor_pin[n] = {A0, A1, A2, A3}; // photoresitor pins (must be analog)
 
 // Program specific variables and objects
-Servo servos[n];
-uint64_t last_lit[n] = {0, 0, 0, 0};
-int last_servo_degrees[n] = {0, 0, 0, 0};
-uint16_t temp_time = 0;
-uint16_t photoresistor_value = 0;
-// Change parameters depending on empirical measurements
-constexpr uint16_t thresholds[n] = {500, 500, 500, 500};
-constexpr uint64_t min_delay_ms = 100;
-constexpr int servo_pressed_degrees = 45;
-constexpr int servo_released_degrees = 0;
+Servo servos[n];                            // Servo object used by the Servo library
+uint64_t last_photoresistor_check[n] = {0}; // Last time the photoresistor value was read, modified in loop
+int last_servo_degrees[n] = {0};            // Last angle the servomotor was set to, modified in loop
+int servo_released_degrees[n] = {0};        // Angle at which the servo's handle doesn't touch the screen, modified in setup
+uint16_t temp_time = 0;                     // Temporary variable that stores time passed between servo presses, modified in loop
+uint16_t photoresistor_value = 0;           // Temporary variable that stores values read by the photoresistor, modified in loop
 
+// ╔───────────────────────╗
+// │Adjust parameters below│
+// ╚───────────────────────╝
+
+// If the measurement value read by photoresistor 'i' is lower than the *threshold* then the servo 'i' moves.
+constexpr uint16_t thresholds[n] = {500, 500, 500, 500}; // Default {500,500,500,500}, Possible integer values 0-1023
+
+// Minimum value of delay between reading from the same photoresistor in miliseconds (1/1000 s).
+constexpr uint64_t min_delay_ms = 100; // Default 100, Possible integer values >0
+
+// The angular displacement ( released angle - pressed angle) for each servomotor's handle when a black tile is detected. Servos spin counterclockwise.
+constexpr int servo_pressed_degrees_delta[n] = {-45, 30, -30, 45}; // Default {-45, 30, -30, 45}, Possible values such that always (released+delta) in [0°; 180°]
+
+// ─────────────────────────
+
+// Only run once
 void setup()
 {
 #ifdef DEBUG
@@ -28,30 +40,61 @@ void setup()
   Serial.begin(9600);
 #endif
 
-  // Assingning Analog pins to read data from photoresistor
-  pinMode(photoresistor_pin[0], INPUT);
-  pinMode(photoresistor_pin[1], INPUT);
-  pinMode(photoresistor_pin[2], INPUT);
-  pinMode(photoresistor_pin[3], INPUT);
+  // Fill program specific arrays with all 0
+  memset(last_photoresistor_check, 0, n * sizeof(uint64_t));
+  memset(last_servo_degrees, 0, n * sizeof(int));
+  memset(servo_released_degrees, 0, n * sizeof(int));
 
-  // Attaching servo pins to allow for control
-  servos[0].attach(servo_pin[0]);
-  servos[1].attach(servo_pin[1]);
-  servos[2].attach(servo_pin[2]);
-  servos[3].attach(servo_pin[3]);
+  delay(5000); // wait 5s
+
+  for (size_t i = 0; i < n; i++)
+  {
+    // Assingning Analog pins to read data from photoresistor
+    pinMode(photoresistor_pin[i], INPUT);
+    // Attaching servo pins to allow for control
+    servos[i].attach(servo_pin[i]);
+    // Setting the default position
+    servo_released_degrees[i] = servos[i].read();
+#ifdef DEBUG
+    // Print the value read by servo to the serial monitor
+    Serial.println("Servo " + String(i) + "default position: " + photoresistor_value);
+#endif
+    // Release all servos
+    servos[i].write(servo_released_degrees[i]);
+  }
+
+  delay(5000); // wait 5s
+
+  // Press down all servo handles at once
+  for (int i = 0; i < n; i++)
+  {
+    servos[i].write(servo_released_degrees[i] + servo_pressed_degrees_delta[i]);
+  }
+
+  // Hold shortly
+  delay(100);
+
+  // Release all servo handles and begin loop
+  for (int i = 0; i < n; i++)
+  {
+    servos[i].write(servo_released_degrees[i]);
+  }
+  delay(100);
 }
 
+// Run indefinitely in a loop
 void loop()
 {
+
   // For each pair `i` we check sequentially that ...
   for (size_t i = 0; i < n; i++)
   {
 
     // if the time since last value read for that specific photoresistor `i` was read
     // is less than min_delay_ms
-    if (((temp_time = millis()) - last_lit[i]) > min_delay_ms)
+    if (((temp_time = millis()) - last_photoresistor_check[i]) > min_delay_ms)
     {
-      last_lit[i] = temp_time; // reset timer
+      last_photoresistor_check[i] = temp_time; // reset timer
 
       // check if the photoresistor `i` value is less than the threshold parameter
       // Note that the threshold value should be adjusted
@@ -59,22 +102,18 @@ void loop()
       if ((photoresistor_value = analogRead(photoresistor_pin[i])) < thresholds[i])
       {
 
-// Print the value read by photoresistor to the serial monitor
 #ifdef DEBUG
-        Serial.print(i);
-        Serial.print(" ");
-        Serial.print(photoresistor_value);
+        // Print the value read by photoresistor to the serial monitor
+        Serial.println("Photores " + String(i) + ": " + String(photoresistor_value));
 #endif
 
-        // Move the servomotor handle to press on the screen (45° default)
-        servos[i].write(servo_pressed_degrees);
-        last_servo_degrees[i] = servo_pressed_degrees;
+        // Move the servomotor handle to press on the screen
+        last_servo_degrees[i] = servo_released_degrees[i] + servo_pressed_degrees_delta[i];
       }
       else
       {
-        // Move back the servomotor handle to release the tap (0° default)
-        servos[i].write(servo_released_degrees);
-        last_servo_degrees[i] = servo_released_degrees;
+        // Move back the servomotor handle to release the tap
+        last_servo_degrees[i] = servo_released_degrees[i];
       }
     }
     // save last handle position until the next check

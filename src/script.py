3ea09116e2697161eeb5b5301e5a85a0cbc07a7e
Tomasz Pawłowski <t.pawlowski03@gmail.com>
2024-05-07 19:17:19 +0200

    simple scrolling and code formatting

--- src/script.py
+++ src/script.py
@@ -2,8 +2,9 @@
 
 # TODO: time scale
 # TODO: wartosci
-# TODO: scroll
 
+# Required dependencies
+# pip install pyserial opencv-python numpy
 import serial
 import numpy as np
 import time
@@ -18,47 +19,54 @@ timestart = 0
 
 port = "/dev/ttyACM0"
 baudRate = 115200
-serialCom = serial.Serial(port, baudRate, timeout=10, write_timeout=10)
-
-
-n = 360_000
-iters = [0, 0, 0, 0]
-
-rows = 20
+serialCom = serial.Serial(port, baudRate, timeout=10, write_timeout=10) # open a serial connection
+n = 360_000 # rows of the history
+rows = 40 #  rows of the preview
+title = "Wizualizacja danych z robota" # title of the window
 
 
 
-# data = np.random.choice([0,122,255],size=(n*rows,4))
-# data = np.random.randint(1024,size=(n*rows,4),dtype=np.uint16)
-data = np.full((n*rows,4),1024,dtype=np.uint16)
-data_slice = np.full((rows,4),0,dtype=np.uint16)
+# data = np.random.randint(1024,size=(n*rows,4),dtype=np.uint16) # mock data
+iters = [0, 0, 0, 0] # pointers to the next row to fill for each column, could be simplified
+data = np.full((n*rows,4),1024,dtype=np.uint16) # full history of measurements
 
-def get_slice(i):
+def get_slice(i): 
+    # Get a slice of data expressed as rbg triplets ready for rendering
+
     i = max(rows,i)
-    part = data[i-rows:i, :]
-    newpart = np.where(part == 1024, 122, np.where(part < thresholds, 0, 255))
-    return np.repeat(newpart[:,:, np.newaxis], 3, axis=2).astype(np.uint8)
+    part = data[i-rows:i, :] # get a slice of data ending at location i
+    newpart = np.where(part == 1024, 122, np.where(part < thresholds, 0, 255)) # convert measurement values to single channel rgb
+    return np.repeat(newpart[:,:, np.newaxis], 3, axis=2).astype(np.uint8)  # convert single channel rgb to tripple and change type to match opencv
 
 
 def animate(line):
-    if line == None:
+    # Convert an incoming line from serial to a frame 
+
+    # line[0] - time
+    # line[1] - column
+    # line[2] - measurement value
+
+    if line == None: # line from serial has corrupted data, just re-render the same frame 
         return get_slice(max(iters))
-    a = line[1]
+    a = line[1] # which photoresistor (column)
     i = max(iters)
-    data[i, a] = line[2]
-    iters[a] += 1
-    i += 1
-    return get_slice(i)
+    data[i, a] = line[2] # fill the correct column at next available row with value from the measurement
+    iters[a] += 1 # increase column pointer
+    return get_slice(i+1) # render a frame with updated data with i+1 row downmost
    
 def read_line_serial():
+    # read a line from serial, none if corrupted (lazy check)
     line = get_photoresistor_measurement(read_bytes_string())
-    # print(line)
-    if line[1] == -1:
+    if line[1] < 0 or line[1] > 3: # column bounds check and exclude c
         return None
+    elif line[2] < 0 or line[2] > 1023: # check if measurements make sense
+        return None
+    # note no time check
     return line
 
 
 def get_photoresistor_measurement(input):
+    # Parse string to get 3 ints or return [-1, -1, 1024]
     if input != "":
         try:
             values = [int(x) for x in input.split()]
@@ -69,6 +77,7 @@ def get_photoresistor_measurement(input):
     return [-1, -1, 1024]
 
 def get_thresholds(input="", thresholds=thresholds):
+    # Parse string to get 4 ints or return default value
     if input != "":
         try:
             values = [int(x) for x in input.split()]
@@ -79,6 +88,7 @@ def get_thresholds(input="", thresholds=thresholds):
     return thresholds
 
 def get_time(input="", time1=delay):
+    # Parse string to get an int or return default value
     if input != "":
         try:
             return int(input)
@@ -87,6 +97,7 @@ def get_time(input="", time1=delay):
     return time1
 
 def get_servos(input="", servos=servos):
+    # Parse string to get 4 ints or return default value
     if input != "":
         try:
             values = [int(x) for x in input.split()]
@@ -98,20 +109,53 @@ def get_servos(input="", servos=servos):
 
 
 def read_bytes_string():
-    # try:
-    s_bytes = serialCom.readline()
-    return s_bytes.decode("ascii").strip()
-    # except serial.SerialException:
-        # return ""
+    # Read incoming bytes from serial as string
+    try:
+        s_bytes = serialCom.readline()
+        return s_bytes.decode("ascii").strip()
+    except serial.SerialException:
+        return ""
+
+
+def live_view():
+    while True:
+            line = read_line_serial()
+            print(line)
+            image = cv2.resize(animate(line), dsize=(1000, 1200), interpolation=cv2.INTER_NEAREST)
+            cv2.imshow(title, image)
+            if cv2.waitKey(1) == 27: # esc
+                scrollable_view()
+
+def scrollable_view():
+    serialCom.close()
+    current_row = max(iters)
+    max_row = current_row+1
+    print("Esc to exit")
+    print("w - scroll up, s - scroll down")
+    while True:
+        image = cv2.resize(get_slice(current_row), dsize=(1000, 1200), interpolation=cv2.INTER_NEAREST)
+        cv2.imshow(title, image)
+        k = cv2.waitKey(1)
+        if k == 27: # esc
+            exit()
+        elif k==ord('w'): # up
+            current_row = max(current_row-1,rows)
+        elif k==ord('s'): # down
+            current_row = min(current_row+1, max_row)
 
 
 def main(thresholds=thresholds, delay=delay, servos=servos):
 
-    # print("Opened port ", port)
+    print("Esc to stop & scroll")
+
+    # Send signal to restart arduino over serial
     time.sleep(2)
     serialCom.flushInput()
     serialCom.setDTR(True)
+    print("Opened port ", port)
 
+    #      Thresholds    Delay     Servos
+    # . 200,200,200,200 ; 100 ; 25,25,25,25\n
     to_arduino = (
         ". "
         + ",".join(map(str, thresholds))
@@ -121,25 +165,29 @@ def main(thresholds=thresholds, delay=delay, servos=servos):
         + ",".join(map(str, servos))
         + "\n"
     )
-    # print(to_arduino, end="")
+    # Send a command to overwrite program parameters (thresholds, delay, servos) using the values defined on the top of the current file
     serialCom.write(to_arduino.encode("ascii"))
     serialCom.flushInput()
 
-    thresholds = get_thresholds(read_bytes_string())
-    delay = get_time(read_bytes_string())
-    servos = get_servos(read_bytes_string())
+    # Read updated values (or not if something went wrong) from serial
+    thresholds = get_thresholds(read_bytes_string(), thresholds)
+    delay = get_time(read_bytes_string(), delay)
+    servos = get_servos(read_bytes_string(), servos)
     timestart = get_time(read_bytes_string(), 0)
 
-cv2.namedWindow("window cv2", cv2.WINDOW_NORMAL)
+
+
+
 if __name__ == "__main__":
     try:
         main()
-        while True:
-            line = read_line_serial()
-            print(line)
-            cv2.imshow("window cv2", animate(line))
-            cv2.waitKey(1)
-    finally:
+        cv2.namedWindow(title, cv2.WINDOW_GUI_NORMAL | cv2.WINDOW_AUTOSIZE)
+        live_view()
+
+    except KeyboardInterrupt:
+        scrollable_view()
+
+    finally: # ALWAYS executes at the end of the program, no matter what, all exceptions inside are ignored
         cv2.destroyAllWindows()
         serialCom.close()
         
